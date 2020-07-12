7579d0040e80bc30eb7b9ba85f1dba92a83afc29
jiyun1006 <kimjiyoon1006@hanmail.net>
2020-07-13 01:00:58 +0900

    9576

--- greedy/baek_9576.py
+++ greedy/baek_9576.py
@@ -5,15 +5,11 @@ for _ in range(t_n):
     N, M = map(int, sys.stdin.readline().split())
     num = [list(map(int, sys.stdin.readline().split())) for i in range(M)]
     num.sort(key=lambda x: x[:][1])
-    N_list = []
-    for i in range(1, N + 1):
-        N_list.append(i)
+    N_list = [0] * (N + 1)
 
-    cnt = 0
     for i in range(M):
         for j in range(num[i][0], num[i][1] + 1):
-            if j in N_list:
-                N_list.remove(j)
-                cnt += 1
+            if N_list[j] == 0:
+                N_list[j] = 1
                 break
-    print(cnt)
+    print(sum(N_list))
