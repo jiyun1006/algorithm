923f239e2f62d0f38e12405bdbc4504efe733bf9
jiyun1006 <52434993+jiyun1006@users.noreply.github.com>
2020-07-27 21:26:33 +0900

    1826

---
+++ greedy/baek_1826.py
@@ -0,0 +1,39 @@
+import sys
+import heapq
+
+io = sys.stdin.readline
+
+N = int(io())
+f = [0] * N
+heap = []
+cnt = 0
+
+for i in range(N):
+    a, b = map(int, io().split())
+    f[i] = (a, b)
+
+f.sort()
+
+L, P = map(int, io().split())
+
+for i in range(len(f)):
+    while P < f[i][0]:
+        if not heap:
+            cnt = -1
+            break
+        P += heapq.heappop(heap)[1]
+        cnt += 1
+    if cnt == -1:
+        break
+    heapq.heappush(heap, (-f[i][1], f[i][1]))
+
+while P < L:
+    if cnt == -1:
+        break
+    if not heap:
+        cnt = -1
+        break
+    P += heapq.heappop(heap)[1]
+    cnt += 1
+
+print(cnt)
