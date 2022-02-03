1da8f1406dc41ca17e737ddc6c496042e6e36154
jiyun1006 <jiyun4251@gmail.com>
2022-02-03 18:43:52 +0900

    📝  brute force (feat. while)

--- baekjoon/brute-force/baek_1075.js
+++ baekjoon/brute-force/baek_1075.js
@@ -1,4 +1,16 @@
 const fs = require("fs");
 const filepath = process.platform === 'linux' ? '/dev/stdin' : '.ex/txt';
-let input = fs.readFileSync("./ex.txt").toString();
+let input = fs.readFileSync("./ex.txt").toString().split('\n');
 
+let N = Number(input.shift());
+const F = input.shift();
+
+N = N - N % 100;
+
+while (N % F !== 0) {
+    N++;
+}
+
+N = N.toString();
+
+console.log(N.slice(-2));
