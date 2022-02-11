b35d8ae1c94a77835c1cbb33bd639bddccd79903
jiyun1006 <jiyun4251@gmail.com>
2022-02-12 00:03:10 +0900

    📝 구현 (feat. dictionary 방식)

--- baekjoon/implementation/baek_1316.js
+++ baekjoon/implementation/baek_1316.js
@@ -3,9 +3,26 @@ const filepath = process.platform === 'linux' ? '/dev/stdin' : '.ex/txt';
 let input = fs.readFileSync("./ex.txt").toString().trim().split('\n');
 
 const num = Number(input.shift());
-
+let ans = 0;
 
 for (let i = 0; i < num; i++) {
+    let word = input.shift();
+    let arr = new Array();
+    let tmp = '';
+    let check = true;
+    for (let i of word) {
+        if (arr.indexOf(i) >= 0) {
 
+            if (tmp === i) continue;
+            else {
+                check = false;
+                break
+            };
+        }
+        arr.push(i);
+        tmp = i;
+    }
+    if (check) ans++;
 }
 
+console.log(ans);
